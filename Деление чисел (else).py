93216866cbedc183c501b1ad7fc2b398e56b22f2
Aigerim Kuderbaeva <152085559+omindspace@users.noreply.github.com>
2023-12-05 00:06:09 +0600

    Деление чисел с Else
    Конструкция If+ else

---
+++ Деление чисел (else).py
@@ -0,0 +1,8 @@
+n = int(input(' Введите 1 число :'))
+m = int(input(' Введите 2 число :'))
+
+if m == 0: print(' Нельзя делить на ноль!')
+else:
+    result = n / m
+    
+    print(n,'/',m,'=', result)
