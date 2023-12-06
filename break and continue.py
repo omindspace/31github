822f88367d3d9cf5b05959672824d11f3017a172
Aigerim Kuderbaeva <152085559+omindspace@users.noreply.github.com>
2023-12-07 00:03:17 +0600

    Работа с функциями
    for,range,break,continue

---
+++ break and continue.py
@@ -0,0 +1,7 @@
+for i in range(20):
+    if i % 2 == 0: continue
+    if i > 10: break 
+    
+    print(i,end='  ')
+
+print('Конец программы')
---
+++ for (range).py
@@ -0,0 +1,15 @@
+for _ in range(5) :
+    print('*', end = '  ')
+print()
+for i in range(5) :
+    print(i, end = '  ')
+print()
+for i in range(5, 10) :
+    print(i, end = '  ')
+print()
+for i in range(10,5,-1) :
+    print(i, end = '  ')
+print()
+for i in range(10,50,5) :
+    print(i, end = '  ')
+
