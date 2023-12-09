96696517f94f5b4dc6b24cd23fe1974171048f96
Aigerim Kuderbaeva <152085559+omindspace@users.noreply.github.com>
2023-12-09 15:44:09 +0600

    Конструктор  match

---
+++ Калькулятор с Math.py
@@ -0,0 +1,24 @@
+print("0 в качестве знака - выход из программы\n")
+ 
+while True:
+    s = input("Знак (+, -, *, /): ")
+    if s in ('+', '-', '*', '/'):
+        a = float(input("a = "))
+        b = float(input("b = "))
+ 
+    math s:
+        case '+':
+            print("%.2f" % (a + b))
+        case '-':
+            print("%.2f" % (a - b))
+        case '*':
+            print("%.2f" % (a * b))
+        case '/':
+            if b != 0:
+                print("%.2f" % (a / b))
+            else:
+                print("Деление на ноль!")
+        case '0':
+            break
+        case _:
+            print("Неверный знак операции!")
